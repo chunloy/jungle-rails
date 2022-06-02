a96311feacf53d5897571da5a697bd786e1700a9
Aaron Au <aaron.au@me.com>
2022-06-02 01:34:22 -0600

    Add admin dashboard counts to a table and link to admin index page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_str, password: ENV['ADMIN_PASSWORD'].to_str
   def show
+    @products = Product.all.length
+    @categories = Category.all.length
+    @line_items = LineItem.all.length
+    @orders = Order.all.length
   end
 end
