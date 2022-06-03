15dbd56f3e6207a22db766c976db32b913d022b1
Aaron Au <aaron.au@me.com>
2022-06-02 21:22:35 -0600

    Set up routes for users with new and create actions

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  resources :users, only: [:new, :create]
 
   namespace :admin do
     root to: 'dashboard#show'
