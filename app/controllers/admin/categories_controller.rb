881d7a0981260fa23fb8c72725fddd8657061974
Aaron Au <aaron.au@me.com>
2022-06-06 23:09:18 -0600

    Fix formatting for admin categories dashboard

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.order(id: :asc).all
   end
 
   def new
